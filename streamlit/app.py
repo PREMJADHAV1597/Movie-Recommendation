8fe3ce896348a8b5b882e5ed7090c8009b3880ce
Prem Ganesh Jadhav <premjadhav370@gmail.com>
2025-08-21 14:38:05 +0530

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -10,7 +10,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 # ----------------------------
 @st.cache_data
 def load_data():
-    df = pd.read_csv("Movies Recommendation.csv")  
+    df = pd.read_csv("streamlit/Movies Recommendation.csv")  
     return df
 
 movies = load_data()
